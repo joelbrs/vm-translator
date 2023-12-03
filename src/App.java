0c654869ee78973dae133a53d0801e36ae0cc6a8
Joel da Silva Pereira Filho <joelfilho028212@gmail.com>
2023-12-03 13:45:09 -0300

    deleted: main & feat: add app

---
+++ src/App.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+public class App {
+    private static String fromFile(File file) {
+        byte[] bytes;
+
+        try {
+            bytes = Files.readAllBytes(file.toPath());
+            return new String(bytes, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static void translateFile (File file, CodeWriter code) {
+        String input = fromFile(file);
+        Parser p = new Parser(input);
+
+        while (p.hasMoreCommands()) {
+            Command command = p.nextCommand();
+            switch (command.getType()) {
+                case ADD:
+                    code.writeArithmeticAdd();
+                    break;
+                case SUB:
+                    code.writeArithmeticSub();
+                    break;
+                case NEG:
+                    code.writeArithmeticNeg();
+                    break;
+                case NOT:
+                    code.writeArithmeticNot();
+                    break;
+                case EQ:
+                    code.writeArithmeticEq();
+                    break;
+                case LT:
+                    code.writeArithmeticLt();
+                    break;
+                case GT:
+                    code.writeArithmeticGt();
+                    break;
+                case AND:
+                    code.writeArithmeticAnd();
+                    break;
+                case OR:
+                    code.writeArithmeticOr();
+                    break;
+                case PUSH:
+                    //code.writePush(command.args.get(0), Integer.parseInt(command.args.get(1)));
+                    break;
+                case POP:
+                    //code.writePop(command.args.get(0), Integer.parseInt(command.args.get(1)));
+                    break;
+                default:
+                    System.out.println(command.getType().toString()+" not implemented");
+            }
+        }
+    }
+}
