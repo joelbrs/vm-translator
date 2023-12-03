2a79293a02dd5f4f5ec60460dfdcadff3b8adc8b
Joel da Silva Pereira Filho <joelfilho028212@gmail.com>
2023-12-03 13:36:31 -0300

    feat: add arithmetic operations in CodeWriter

--- src/CodeWriter.java
+++ src/CodeWriter.java
@@ -1,6 +1,10 @@
 import enums.Segments;
 import utils.WriteUtils;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class CodeWriter implements WriteUtils {
 
     private final StringBuilder output = new StringBuilder();
@@ -70,6 +74,217 @@ public class CodeWriter implements WriteUtils {
         write(output, "@R13");
         write(output, "A=M");
         write(output, "M=D");
+    }
+
+    void writeArithmeticAdd()
+    {
+        write(output, "@SP // add");
+        write(output, "M=M-1");
+        write(output, "A=M");
+        write(output, "D=M");
+        write(output, "A=A-1");
+        write(output, "M=D+M");
+    }
+
+    void writeArithmeticSub()
+    {
+        write(output, "@SP // sub");
+        write(output, "M=M-1");
+        write(output, "A=M");
+        write(output, "D=M");
+        write(output, "A=A-1");
+        write(output, "M=M-D");
+    }
+
+    void writeArithmeticNeg()
+    {
+        write(output, "@SP // neg");
+        write(output, "A=M");
+        write(output, "A=A-1");
+        write(output, "M=-M");
+    }
+
+
+    void writeArithmeticAnd()
+    {
+        write(output, "@SP // and");
+        write(output, "AM=M-1");
+        write(output, "D=M");
+        write(output, "A=A-1");
+        write(output, "M=D&M");
+    }
+
+    void writeArithmeticOr()
+    {
+        write(output, "@SP // or");
+        write(output, "AM=M-1");
+        write(output, "D=M");
+        write(output, "A=A-1");
+        write(output, "M=D|M");
+    }
+
+    void writeArithmeticNot()
+    {
+
+        write(output, "@SP // not");
+        write(output, "A=M");
+        write(output, "A=A-1");
+        write(output, "M=!M");
+    }
+
+
+    void writeArithmeticEq()
+    {
+        String label=("JEQ_" + moduleName + "_" + (labelCount));
+        write(output, "@SP // eq");
+        write(output, "AM=M-1");
+        write(output, "D=M");
+        write(output, "@SP");
+        write(output, "AM=M-1");
+        write(output, "D=M-D");
+        write(output, "@" + label);
+        write(output, "D;JEQ");
+        write(output, "D=1");
+        write(output, "(" + label + ")");
+        write(output, "D=D-1");
+        write(output, "@SP");
+        write(output, "A=M");
+        write(output, "M=D");
+        write(output, "@SP");
+        write(output, "M=M+1");
+
+        labelCount++;
+    }
+
+    void writeArithmeticGt()
+    {
+        String labelTrue=("JGT_TRUE_" + moduleName + "_" + (labelCount));
+        String labelFalse=("JGT_FALSE_" + moduleName + "_" + (labelCount));
+
+        write(output, "@SP // gt");
+        write(output, "AM=M-1");
+        write(output, "D=M");
+        write(output, "@SP");
+        write(output, "AM=M-1");
+        write(output, "D=M-D");
+        write(output, "@" + labelTrue);
+        write(output, "D;JGT");
+        write(output, "D=0");
+        write(output, "@" + labelFalse);
+        write(output, "0;JMP");
+        write(output, "(" + labelTrue + ")");
+        write(output, "D=-1");
+        write(output, "(" + labelFalse + ")");
+        write(output, "@SP");
+        write(output, "A=M");
+        write(output, "M=D");
+        write(output, "@SP");
+        write(output, "M=M+1");
+
+        labelCount++;
+    }
+
+    void writeArithmeticLt()
+    {
+        String labelTrue = ("JLT_TRUE_" + moduleName + "_" + (labelCount));
+        String labelFalse = ("JLT_FALSE_" + moduleName + "_" + (labelCount));
+
+        write(output, "@SP // lt");
+        write(output, "AM=M-1");
+        write(output, "D=M");
+        write(output, "@SP");
+        write(output, "AM=M-1");
+        write(output, "D=M-D");
+        write(output, "@" + labelTrue);
+        write(output, "D;JLT");
+        write(output, "D=0");
+        write(output, "@" + labelFalse);
+        write(output, "0;JMP");
+        write(output, "(" + labelTrue + ")");
+        write(output, "D=-1");
+        write(output, "(" + labelFalse + ")");
+        write(output, "@SP");
+        write(output, "A=M");
+        write(output, "M=D");
+        write(output, "@SP");
+        write(output, "M=M+1");
+
+        labelCount++;
+    }
+
+    void  writeLabel(String label ) {
+        write(output, "(" + label + ")");
+    }
+
+    void  writeGoto(String label) {
+        write(output, "@" + label);
+        write(output, "0;JMP");
+    }
+
+    void  writeIf(String label ) {
+        write(output, "@SP");
+        write(output, "AM=M-1");
+        write(output, "D=M");
+        write(output, "M=0");
+        write(output, "@" + label);
+        write(output, "D;JNE");
+
+    }
+
+    void  writeFunction(String funcName , int nLocals ) {
+
+        var loopLabel = funcName + "_INIT_LOCALS_LOOP";
+        var loopEndLabel = funcName + "_INIT_LOCALS_END";
+
+
+        write(output, "(" + funcName + ")" + "// initializa local variables");
+        write(output, String.format("@%d", nLocals));
+        write(output, "D=A");
+        write(output, "@R13"); // temp
+        write(output, "M=D");
+        write(output, "(" + loopLabel + ")");
+        write(output, "@" + loopEndLabel);
+        write(output, "D;JEQ");
+        write(output, "@0");
+        write(output, "D=A");
+        write(output, "@SP");
+        write(output, "A=M");
+        write(output, "M=D");
+        write(output, "@SP");
+        write(output, "M=M+1");
+        write(output, "@R13");
+        write(output, "MD=M-1");
+        write(output, "@" + loopLabel);
+        write(output, "0;JMP");
+        write(output, "(" + loopEndLabel + ")");
+    }
+
+    void  writeFramePush(String value) {
+        write(output, "@" + value);
+        write(output, "D=M");
+        write(output, "@SP");
+        write(output, "A=M");
+        write(output, "M=D");
+        write(output, "@SP");
+        write(output, "M=M+1");
+    }
+
+    public String codeOutput() {
+        return output.toString();
+    }
+
+    public void save() {
+        FileOutputStream outputStream;
+        try {
+            outputStream = new FileOutputStream(outputFileName);
+
+            outputStream.write(output.toString().getBytes());
+
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
